9b8d2ba1fa0807e55fb55def667a227f28d17ac3
amrita0923 <amrita.sumi@gmail.com>
2025-10-29 11:37:44 +0530

    first commit

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("welcome to healthy program life ")
+#displaying the user with warm welcome
+print("=========*******welocome to healthy life program*******============")
+
+#Taking user weight
+weight=int(input("Enter the wight in (kgs): "))
+
+#Taking user height
+height= int(input("Enter the height in (cms): "))
+
+#displaying his BMI
+print(f"--------------your weight is {weight}kgs and your height is {height}cms--------------")
