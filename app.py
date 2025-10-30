9a8f32f3efc2ed91017c57d9cdc3ce613512bb41
amrita0923 <amrita.sumi@gmail.com>
2025-10-30 11:21:40 +0530

    bmi calculating

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+from logic import BMI
 #displaying the user with warm welcome
-print("=========*******welocome to healthy life program*******============")
+print("=========*******welcome to healthylife program********============")
 
 #Taking user weight
-weight=int(input("Enter the wight in (kgs): "))
+weight=float(input("Enter the wight in (kgs): "))
 
 #Taking user height
-height= int(input("Enter the height in (cms): "))
+height= float(input("Enter the height in (ms): "))
+
+#calling BMI function
+bmi=BMI(weight,height)
 
 #displaying his BMI
-print(f"--------------your weight is {weight}kgs and your height is {height}cms--------------")
+print(f"Your BMI is {bmi}")
---
+++ logic.py
@@ -0,0 +1,4 @@
+#BMIL logic
+def BMI(weight,height):
+    bmi=(weight)/(height)**2
+    return bmi
